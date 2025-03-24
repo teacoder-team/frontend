2dd82ecb2fdf2f1cd3df3712bf728b0e10b720c8
TeaCoder52 <vnechaev888@gmail.com>
2025-03-24 20:40:22 +0300

    fix: hero section

--- src/components/home/hero.tsx
+++ src/components/home/hero.tsx
@@ -5,7 +5,7 @@ import { Button } from '../ui/button'
 
 export function Hero() {
 	return (
-		<section className='container flex min-h-[calc(100vh-6.5rem)] max-w-screen-2xl flex-col items-center justify-center space-y-6 py-16 text-center sm:space-y-8 sm:py-20 md:py-28 lg:py-32'>
+		<section className='mx-auto flex min-h-[calc(100vh-6.5rem)] max-w-screen-2xl flex-col items-center justify-center space-y-6 py-16 text-center sm:space-y-8 sm:py-20 md:py-28 lg:py-32'>
 			<div className='space-y-4'>
 				<h1 className='bg-gradient-to-br from-foreground from-30% via-foreground/90 to-foreground/70 bg-clip-text text-4xl font-bold text-transparent sm:text-5xl md:text-6xl lg:text-7xl lg:leading-[80px]'>
 					Изучай веб-разработку
