ab15a1a9db03d54681307c0f8cfbeb5618bbbaac
TeaCoder52 <vnechaev888@gmail.com>
2025-10-18 09:00:42 +0300

    feat(payment): separate available and upcoming methods

--- src/constants/payment-methods.ts
+++ src/constants/payment-methods.ts
@@ -1,7 +1,7 @@
-import { BitcoinIcon, CreditCardIcon } from 'lucide-react'
+import { BitcoinIcon, CreditCardIcon, GlobeIcon, StarIcon } from 'lucide-react'
 import type { ComponentType, SVGProps } from 'react'
 
-import { SbpIcon, YoomoneyIcon } from '../components/icons'
+import { SbpIcon } from '../components/icons'
 
 import { InitPaymentRequestMethod } from '@/src/api/generated'
 
@@ -10,43 +10,51 @@ interface PaymentMethod {
 	name: string
 	description: string
 	icon: ComponentType<SVGProps<SVGSVGElement>>
-	isAllowed?: boolean
+	isAvailable?: boolean
 }
 
 export const PAYMENT_METHODS: PaymentMethod[] = [
 	{
 		id: InitPaymentRequestMethod.BANK_CARD,
 		name: 'Банковская карта',
-		description: 'Оплата кредитной или дебетовой картой',
+		description: 'Оплата картой российских банков',
 		icon: CreditCardIcon,
-		isAllowed: true
+		isAvailable: true
 	},
 	{
 		id: InitPaymentRequestMethod.SBP,
 		name: 'СБП',
 		description: 'Оплата через Систему быстрых платежей',
 		icon: SbpIcon,
-		isAllowed: true
-	},
-	{
-		id: InitPaymentRequestMethod.YOOMONEY,
-		name: 'ЮMoney',
-		description: 'Оплата через кошелек ЮMoney',
-		icon: YoomoneyIcon,
-		isAllowed: true
+		isAvailable: true
 	},
 	// {
-	// 	id: InitPaymentRequestMethod.STARS,
-	// 	name: 'Telegram Stars',
-	// 	description: 'Оплата подписки через звёзды Telegram',
-	// 	icon: StarIcon,
-	// 	isAllowed: true
+	// 	id: InitPaymentRequestMethod.YOOMONEY,
+	// 	name: 'ЮMoney',
+	// 	description: 'Оплата через кошелек ЮMoney',
+	// 	icon: YoomoneyIcon,
+	// 	isAvailable: true
 	// },
+
 	{
 		id: 'CRYPTO',
 		name: 'Криптовалюта',
 		description: 'Оплата с помощью BTC, USDT, TON',
 		icon: BitcoinIcon,
-		isAllowed: true
+		isAvailable: true
+	},
+	{
+		id: InitPaymentRequestMethod.INTERNATIONAL_CARD,
+		name: 'Международные карты',
+		description: 'Оплата картой зарубежных банков',
+		icon: GlobeIcon,
+		isAvailable: false
+	},
+	{
+		id: InitPaymentRequestMethod.STARS,
+		name: 'Telegram Stars',
+		description: 'Оплата подписки через звёзды Telegram',
+		icon: StarIcon,
+		isAvailable: false
 	}
 ]
