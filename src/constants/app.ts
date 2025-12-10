5cc8ac6375a504aee4afe46941a92f3b386e6885
TeaCoder52 <vnechaev888@gmail.com>
2025-12-10 06:49:07 +0300

    fix: resolve build errors

--- src/constants/app.ts
+++ src/constants/app.ts
@@ -1,5 +1,5 @@
 export const APP_CONFIG = {
-	baseUrl: process.env['NEXT_PUBLIC_APP_URL'] as string,
+	baseUrl: process.env['NEXT_PUBLIC_APP_URL'] ?? 'https://teacoder.ru',
 	apiUrl: process.env['NEXT_PUBLIC_API_URL'] as string,
 	storageUrl: process.env['NEXT_PUBLIC_STORAGE_URL'] as string
 } as const
