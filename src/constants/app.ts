b5e4a2f122c6efe0669dc2b68d72abd6f99dfe81
Vadim <vnechaev888@gmail.com>
2026-09-20 18:16:35 +0300

    chore: update dockerfile

--- src/constants/app.ts
+++ src/constants/app.ts
@@ -1,6 +1,6 @@
 export const APP_CONFIG = {
-	baseUrl: process.env['NEXT_PUBLIC_APP_URL'] ?? 'https://teacoder.ru',
-	apiUrl: process.env['NEXT_PUBLIC_API_URL'] ?? 'https://api.teacoder.ru',
+	baseUrl: process.env['NEXT_PUBLIC_APP_URL'] || 'https://teacoder.ru',
+	apiUrl: process.env['NEXT_PUBLIC_API_URL'] || 'https://api.teacoder.ru',
 	storageUrl:
-		process.env['NEXT_PUBLIC_STORAGE_URL'] ?? 'https://orion.teacoder.ru'
+		process.env['NEXT_PUBLIC_STORAGE_URL'] || 'https://orion.teacoder.ru'
 } as const
